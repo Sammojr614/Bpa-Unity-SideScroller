4c378097c81103d7b66fd77b81928f9183274f15
Epiczd <nutball37@gmail.com>
2019-09-26 18:02:50 -0400

    Re-added health/movement scripts
    These scripts are not applied yet.

---
+++ Descent into Ere/Assets/Scripts/Movement/Move2D.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Move2D : MonoBehaviour
+{
+    //The speed the player moves
+    public float moveSpeed = 5f;
+    //Checks if the player is on the ground
+    public bool isGrounded = false;
+   
+    //Allows the player to move
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+		transform.position += movement * Time.deltaTime * moveSpeed;
+
+
+    }
+
+    //Allows the player to jump
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+        
+    }
+}
