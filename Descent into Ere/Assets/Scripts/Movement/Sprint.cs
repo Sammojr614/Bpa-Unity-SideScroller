8cfe02075cf7149d9faa3b44b70877d3a7334c2d
Epiczd <nutball37@gmail.com>
2019-09-26 18:38:47 -0400

    Added sprint script
    Use Left Shift to sprint.

---
+++ Descent into Ere/Assets/Scripts/Movement/Sprint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprint : MonoBehaviour {
+
+    public float sprintSpeed = 8f;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            transform.position += movement * Time.deltaTime * sprintSpeed;
+        }
+
+	}
+}
