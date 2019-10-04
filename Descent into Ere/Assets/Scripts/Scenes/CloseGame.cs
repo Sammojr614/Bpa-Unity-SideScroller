1453f7f7dcf150a7922ee0c596c7ad9755288fc5
Epiczd <nutball37@gmail.com>
2019-10-04 08:56:53 -0400

    Made it possible to exit game on title.

---
+++ Descent into Ere/Assets/Scripts/Scenes/CloseGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseGame : MonoBehaviour {
+
+    //Activates after a collision trigger
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        //Checks if colliding object is the player
+        if (other.CompareTag("Player"))
+        {
+            //Quits Game
+            Application.Quit();
+        }
+    }
+}
