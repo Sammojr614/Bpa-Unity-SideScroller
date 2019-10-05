a97c173a614bf58cc5c0773dd89b11767f3b67c5
Sam <sammojr614@gmail.com>
2019-10-04 22:37:36 -0400

    animations Blend Now

--- Descent into Ere/Assets/Scripts/Movement/Move2D.cs
+++ Descent into Ere/Assets/Scripts/Movement/Move2D.cs
@@ -8,30 +8,64 @@ public class Move2D : MonoBehaviour
     //The speed the player moves
     public float moveSpeed = 5f;
     //Checks if the player is on the ground
-    public bool isGrounded ;
+    public bool isGrounded;
+	//Jumping Animation
+	float isJumping;
+	// Moving Float
+	 float isMoving;
+
+	//Giving the Program The Animator
+	public Animator PlayerAnimator;
+	//Getting The Animator
+	void Start(){
+		PlayerAnimator = GetComponent<Animator> ();
+	}
+
 
     /* Allows the player to move, by using l/r arrow keys 
      * Or wasd on the keyboard 
      * (a left, d right)
      */
+	void OzulMoving(){
+		PlayerAnimator.SetFloat ("Moving", 0.5f);
+	}
+
     void Update()
     {
         Jump();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
 		transform.position += movement * Time.deltaTime * moveSpeed;
+		//Walking Animation Perams
+		if (Input.GetButtonDown ("Horizontal")) {
+			OzulMoving ();
+		} else {
+			if (Input.GetButtonUp ("Horizontal")) {
+				PlayerAnimator.SetFloat ("Moving", 0.0f);
+			}
+			//Jumping Animation Perams
+			if (Input.GetButtonDown ("Vertical")) {
+				PlayerAnimator.SetFloat ("Jumping", 1.0f);
+			} else {
+				if (Input.GetButtonDown ("Vertical")) {
+					PlayerAnimator.SetFloat ("Jumping", 0.0f);
+				}
+			}
+		}
 
 
+
+		}
 	
-    }
+    
 
     //Allows the player to jump, if they are on the ground
-    void Jump()
+  	void Jump()
     {
-        if (Input.GetButtonDown("Jump") && isGrounded == true)
-        {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 8f), ForceMode2D.Impulse);
+		if (Input.GetButtonDown ("Jump") && isGrounded == true) {
+			
+			gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0f, 8f), ForceMode2D.Impulse);
 
-        }
+		}
         
     }
 }
--- Descent into Ere/Assets/Scripts/Movement/Sprint.cs
+++ Descent into Ere/Assets/Scripts/Movement/Sprint.cs
@@ -7,25 +7,30 @@ public class Sprint : MonoBehaviour
 
     //Speed the character moves while sprinting
     public float sprintSpeed = 8f;
-
-    void Update()
+	public Animator PlayerAnimator;
+	void Start(){
+		PlayerAnimator = GetComponent<Animator> ();
+	}    
+	void Update()
     {
         /* Allows player to sprint if they
          * Press the left shift button
          */
         if (Input.GetKey(KeyCode.LeftShift))
         {
+			PlayerAnimator.SetFloat ("Moving", 1.0f);
             Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
             transform.position += movement * Time.deltaTime * sprintSpeed;
 		
         }
+
 
         /* Allows player to sprint if they
          * Press the right shift button
          */
         if (Input.GetKey(KeyCode.RightShift))
         {
-			
+			PlayerAnimator.SetFloat ("Moving", 1.0f);	
             Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
             transform.position += movement * Time.deltaTime * sprintSpeed;
 
