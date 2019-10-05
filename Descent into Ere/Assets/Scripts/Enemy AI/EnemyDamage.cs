fc7a53f90b6ae06129c24e85b7f986a4d86fa67a
Epiczd <nutball37@gmail.com>
2019-10-05 17:34:54 -0400

    Fixed ozul in Level Testing

--- Descent into Ere/Assets/Scripts/Enemy AI/EnemyDamage.cs
+++ Descent into Ere/Assets/Scripts/Enemy AI/EnemyDamage.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
-    
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D _collision)
     {
-        
+        if (_collision.gameObject.tag=="Player")
+        {
+            gameObject.GetComponent<PlayerHealth>().health = gameObject.GetComponent<PlayerHealth>().health - 1;
+        }
     }
 }
