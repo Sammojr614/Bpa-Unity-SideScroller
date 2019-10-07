f94f6af1d70555332345f29e1589ea93e8844813
Sammojr614 <Sammojr614@gmail.com>
2019-10-07 10:46:21 -0400

    jump animation

--- Descent into Ere/Assets/Scripts/Movement/Move2D.cs
+++ Descent into Ere/Assets/Scripts/Movement/Move2D.cs
@@ -5,89 +5,89 @@ using UnityEngine;
 
 public class Move2D : MonoBehaviour
 {
-    //The speed the player moves
-    public float moveSpeed = 5f;
-    //Checks if the player is on the ground
-    public bool isGrounded;
+	//The speed the player moves
+	public float moveSpeed = 5f;
+	//Checks if the player is on the ground
+	public bool isGrounded;
 	//Jumping Animation
 	float isJumping;
 	// Moving Float
-	 float isMoving;
+	float isMoving;
 
 	//Giving the Program The Animator
 	public Animator PlayerAnimator;
 	//Getting The Animator
-	void Start(){
-		PlayerAnimator = GetComponent<Animator> ();
-		isJumping = PlayerAnimator.GetFloat ("Jumping");
+	void Start()
+	{
+		PlayerAnimator = GetComponent<Animator>();
+		isJumping = PlayerAnimator.GetFloat("Jumping");
 	}
 
 
-    /* Allows the player to move, by using l/r arrow keys 
+	/* Allows the player to move, by using l/r arrow keys 
      * Or wasd on the keyboard 
      * (a left, d right)
      */
-	void OzulMoving(){
-		PlayerAnimator.SetFloat ("Moving", 0.5f);
+	void OzulMoving()
+	{
+		PlayerAnimator.SetFloat("Moving", 0.5f);
 	}
 
-    void Update()
+	void Update()
 	{
-		Jump ();
-		Fallng ();
-		Vector3 movement = new Vector3 (Input.GetAxis ("Horizontal"), 0f, 0f);
+		Jump();
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
 		transform.position += movement * Time.deltaTime * moveSpeed;
 		//Walking Animation Perams
-		if (Input.GetButtonDown ("Horizontal")) {
-			OzulMoving ();
-		} else {
-			if (Input.GetButtonUp ("Horizontal")) {
-				PlayerAnimator.SetFloat ("Moving", 0.0f);
+		if (Input.GetButtonDown("Horizontal"))
+		{
+			OzulMoving();
+		}
+		else
+		{
+			if (Input.GetButtonUp("Horizontal"))
+			{
+				PlayerAnimator.SetFloat("Moving", 0.0f);
 			}
 
-		
-	}
+
+		}
 		//Telling the Animator When Ozul is Grounded
-		if (isGrounded == true) {
-			PlayerAnimator.SetBool ("IsGrounded", true);
-		} else {
-			if (isGrounded == false) {
-				PlayerAnimator.SetBool ("IsGrounded", false);
+		if (isGrounded == true)
+		{
+			PlayerAnimator.SetBool("IsGrounded", true);
+		}
+		else
+		{
+			if (isGrounded == false)
+			{
+				PlayerAnimator.SetBool("IsGrounded", false);
 			}
 		}
-	}   
-
-
-	void Fallng(){
-		//Falling Animation Perams
-		if (isGrounded == false && Input.GetButtonUp ("Jump")) {
-			PlayerAnimator.SetFloat ("Jumping", 1.0f);
-		} 
 	}
 
 
-    
-
-    //Allows the player to jump, if they are on the ground
-  	void Jump()
-    {
-		if (Input.GetButtonDown ("Jump") && isGrounded == true) {
-			PlayerAnimator.SetFloat ("Jumping", 0.5f);
-			gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0f, 8f), ForceMode2D.Impulse);
-			//Debuging
-			Debug.Log (PlayerAnimator.GetBool ("IsGrounded"));
-			Debug.Log (PlayerAnimator.GetFloat ("Jumping"));
 
 
-		} else {
-			if (Input.GetButtonUp ("Jump") && isGrounded == false) {
-				PlayerAnimator.SetFloat ("Jumping", 1.0f);
+	//Allows the player to jump, if they are on the ground
+	void Jump()
+	{
+		if (Input.GetButtonDown("Jump") && isGrounded == true)
+		{
+			PlayerAnimator.SetFloat("Jumping", 0.0f);
+			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 8f), ForceMode2D.Impulse);
+			Debug.Log(PlayerAnimator.GetBool("IsGrounded"));
+
+		}
+		else
+		{
+			if (Input.GetButtonUp("Jump"))
+			{
+				PlayerAnimator.SetFloat("Jumping", 1.0f);
 			}
-		}
+
+
 
 		}
-	
-
-        
-    }
-
+	}
+}
--- Descent into Ere/Assets/Scripts/Scenes/MoveScenes2D.cs
+++ Descent into Ere/Assets/Scripts/Scenes/MoveScenes2D.cs
@@ -14,11 +14,14 @@ public class MoveScenes2D : MonoBehaviour {
     //Moves scene when player collides with the trigger
     void OnTriggerEnter2D(Collider2D other)
     {
-        //Checks to see if the player has the "Player" tag
-        if (other.CompareTag("Player"))
-        {
-            //Loads desired scene
-            SceneManager.LoadScene(newLevel);
-        }
-    }
+		//Checks to see if the player has the "Player" tag
+		if (other.CompareTag("Player")) {
+
+			//Loads desired scene
+			SceneManager.LoadScene(newLevel);
+
+		}
+	
+
+	}
 }
