db8085d09498d7fe7a5ccc9736351b45e97a2635
Sammojr614 <Sammojr614@gmail.com>
2019-10-10 10:24:34 -0400

    lives animation

--- Descent into Ere/Assets/Scripts/Health/PlayerLives.cs
+++ Descent into Ere/Assets/Scripts/Health/PlayerLives.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class PlayerLives : MonoBehaviour {
 
 	public static int Lives;
-	public Animator PlayerAnimatior;
+	public Animator livesAnimator;
 	void Start () {
 		Lives = 3;
-		PlayerAnimatior = GetComponent<Animator>();
+		 livesAnimator= GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		livesAnimator.SetFloat("SetLives",0.0f);
 		if(PlayerHealth.health == 0){
 			Lives--;
+			
 			PlayerHealth.health = 3;
 		}
 		if(Lives < 0){
 			Lives = 0;
 		}
+		if(Lives == 3){
+			livesAnimator.SetFloat("SetLives", 0.0f);
+		}else{
+		if(Lives == 2){
+			livesAnimator.SetFloat("SetLives", 0.5f);
+		}else{
+			if(Lives == 1){
+				livesAnimator.SetFloat("SetLives", 1.0f);
+			}
+		}
+		}
+
 	}
 }
