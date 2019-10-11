31ded028b3ae042549e059c5a9bc8c70a76ded63
Sammojr614 <Sammojr614@gmail.com>
2019-10-11 08:53:57 -0400

    title Stuff

---
+++ Descent into Ere/Assets/Scripts/OnClickEvents/StartClickSensor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartClickSensor : MonoBehaviour {
+
+	public static bool clicked;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			clicked = true;
+		}else{
+			if(Input.GetMouseButtonUp(0)){
+				clicked = false;
+			}
+		}
+	}
+}
---
+++ Descent into Ere/Assets/Scripts/OnClickEvents/TitleClickEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleClickEvent : MonoBehaviour {
+
+	public Animator TitleAnimator;
+	void Start () {
+		TitleAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(StartClickSensor.clicked == true){
+			TitleAnimator.SetBool("StartButtonClicked", true);
+		}
+	}
+}
