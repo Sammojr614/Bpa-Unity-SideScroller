be68ea6925f188c2a18268dc845d78f6e5e0d96d
Sammojr614 <Sammojr614@gmail.com>
2019-10-11 09:08:07 -0400

    title Scripts

---
+++ Descent into Ere/Assets/Scripts/OnClickEvents/BeginGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeginGame : MonoBehaviour {
+	SpriteRenderer BeginGameSprite;
+	
+	void Start () {
+		BeginGameSprite = gameObject.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			if(StartClickSensor.clicked == true){
+				BeginGameSprite.enabled = true;
+			}else{
+				if(StartClickSensor.clicked == false){
+					BeginGameSprite.enabled = false;
+				}
+			}
+			if(Input.GetMouseButtonDown(0)){
+				SceneManager.LoadScene("MainHub");
+			}
+	}
+}
--- Descent into Ere/Assets/Scripts/OnClickEvents/StartClickSensor.cs
+++ Descent into Ere/Assets/Scripts/OnClickEvents/StartClickSensor.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class StartClickSensor : MonoBehaviour {
 
 	public static bool clicked;
+	SpriteRenderer startButton;
 	void Start () {
-		
+		startButton = gameObject.GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetMouseButtonDown(0)){
 			clicked = true;
+			startButton.enabled = false;
 		}else{
 			if(Input.GetMouseButtonUp(0)){
 				clicked = false;
