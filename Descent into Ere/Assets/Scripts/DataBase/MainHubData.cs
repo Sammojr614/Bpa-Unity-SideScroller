d883884b65ab1f2fad64419d73ed7f1870755e6c
Sam <sammojr614@gmail.com>
2019-10-15 13:48:17 -0400

    Smoothing out some rough Edges

---
+++ Descent into Ere/Assets/Scripts/DataBase/MainHubData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class MainHubData : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		string connectionString = "Data Source = DieDatabase.db;Version=3;";
+		IDbConnection dbCon = new SqliteConnection (connectionString);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
