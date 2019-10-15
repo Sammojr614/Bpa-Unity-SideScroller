0caf1ea81eff97eefda73fd3cd8c65f6382f0a3b
Sammojr614 <Sammojr614@gmail.com>
2019-10-15 09:58:13 -0400

    DataBase management

---
+++ Descent into Ere/Assets/Scripts/DataBase/dataBaseMananger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System.IO;
+
+public class dataBaseMananger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		IDbCommand dbCommand;
+		IDataReader dbReader;
+		string dataCon = "URL=file:"+ Application.persistentDataPath + "/Bpa-Project_DataBase/DieDatabase.db";
+		IDbConnection dbCon  = new SqliteConnection(dataCon);
+		dbCon.Open();
+
+		
+		/*string selectTable = "SELECT*FROM PlayerSaveData";
+		dbCommand = dbCon.CreateCommand();
+		dbCommand.CommandText = selectTable;
+		dbReader = dbCommand.ExecuteReader();*/
+		
+	
+		
+
+		
+	}
+}
+	
+	
