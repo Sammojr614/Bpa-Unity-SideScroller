a22464a2f6d953833600f4b8d8f1a425ae134eaa
Sammojr614 <Sammojr614@gmail.com>
2019-10-15 10:44:39 -0400

    Database

---
+++ Descent into Ere/Assets/Scripts/DataBase/TitleData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.IO;
+using System.Data;
+
+public class TitleData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Connecting to Database
+		string connectionString = "Data Soruce=file:" + Application.persistentDataPath + "/Bpa-Project_Database/DieDatabase.db";
+		 IDbConnection dbCon = new SqliteConnection(connectionString);
+		Debug.Log(connectionString);
+		 
+		 
+	}
+}
+	
