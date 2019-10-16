180db83b4e380b8623062d5fd1011caa1fad56a5
Sam <sammojr614@gmail.com>
2019-10-16 17:15:32 -0400

    merging tuff

--- Descent into Ere/Assets/Scripts/DataBase/PlayerDreamData.cs
+++ Descent into Ere/Assets/Scripts/DataBase/PlayerDreamData.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Data;
-using Mono.Data.Sqlite;
 using System.IO;
-public class MainHubData : MonoBehaviour {
+using Mono.Data.Sqlite;
+
+public class PlayerDreamData : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 		IDbConnection dbCon = new SqliteConnection (TitleData.connectionString);
 		dbCon.Open ();
 		IDbCommand dbCmd = dbCon.CreateCommand ();
-		string updateLocation = "UPDATE PlayerSaveData SET PlayerLocation='MainHub'";
+		string updateLocation = "UPDATE PlayerSaveData SET PlayerLocation='PlayerDream'";
 		dbCmd.CommandText = updateLocation;
 		dbCmd.ExecuteNonQuery ();
 		dbCon.Close ();
--- Descent into Ere/Assets/Scripts/DataBase/TitleData.cs
+++ Descent into Ere/Assets/Scripts/DataBase/TitleData.cs
@@ -1,35 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mono.Data.Sqlite;
-using System.IO;
 using System.Data;
+using System.IO;
+using Mono.Data.Sqlite;
 
 public class TitleData : MonoBehaviour {
-
+	public static string connectionString ="Data Source=DieDataBase.db;";
 	// Use this for initialization
 	void Start () {
-		//Connecting to Database
+		IDbConnection dbCon = new SqliteConnection (connectionString);
+		IDbCommand dbCmd = dbCon.CreateCommand ();
+		dbCon.Open ();
+		string updateLocation = "UPDATE PlayerSaveData SET PlayerLocation='Title'";
+		dbCmd.CommandText = updateLocation;
+		dbCmd.ExecuteNonQuery ();
+		dbCon.Close ();
 
-		string connectionString = "Data Source=DieDataBase.db;";
-		 IDbConnection dbCon = new SqliteConnection(connectionString);
-		 dbCon.Open();
-		 IDbCommand dbCmd = dbCon.CreateCommand();
-		//Checking if there is Data There
-		 string levelDataCheck = "SELECT*FROM LevelData WHERE Level1 = 'Incomplete'";
-		 dbCmd.CommandText = levelDataCheck;
-		 dbCmd.ExecuteNonQuery();
-		  //Adding Data To LevelData
-		 string levelQury = "INSERT INTO LevelData(Level1,Level2,Level3) VALUES('Incomplete','Incomplete','Incomplete')";
-		 dbCmd.CommandText = levelQury;
-		 dbCmd.ExecuteNonQuery();
-		 string playerQury = "INSERT INTO PlayerSaveData(PlayerLocation,PlayerItems,PlayerLives) VALUES('Title','None', '0')";
-		 dbCmd.CommandText = playerQury;
-		 dbCmd.ExecuteNonQuery();
-		 dbCon.Close();
-		 
-		 
-		 
 	}
 }
-	
