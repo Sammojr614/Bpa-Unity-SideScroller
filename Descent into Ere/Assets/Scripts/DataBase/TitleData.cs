fa49af87a4628b7793fa080fe891d645638ea3e1
Sammojr614 <Sammojr614@gmail.com>
2019-10-16 09:06:39 -0400

    Data test

--- Descent into Ere/Assets/Scripts/DataBase/TitleData.cs
+++ Descent into Ere/Assets/Scripts/DataBase/TitleData.cs
@@ -10,19 +10,12 @@ public class TitleData : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		//Connecting to Database
-		string connectionString = "Data Source=DieDatabase.db";
+
+		string connectionString = "Data Source=DieDataBase.db;";
 		 IDbConnection dbCon = new SqliteConnection(connectionString);
-		dbCon.Open ();
-		//Commands
-		IDbCommand dbCmd = dbCon.CreateCommand ();
-		string findTable = "SELECT FROM PlayerSaveData";
-		dbCmd.CommandText = findTable;
-		//reader
-		//Inserting Data
-		string dataInsert = "INSERT INTO \"main\".\"LevelsComplete\"(\"PlayerDream\",\"Level2\",\"Level3\") VALUES (NULL,NULL,NULL)";
-		dbCmd.CommandText = dataInsert;
-
-
+		 dbCon.Open();
+		 IDbCommand dbCmd = dbCon.CreateCommand();
+		 dbCmd.CommandText = "INSERT INTO LevelData(Level1,Level2,Level3) VALUES('Incomplete','Incomplete','Incomplete')";
 		 
 		 
 	}
