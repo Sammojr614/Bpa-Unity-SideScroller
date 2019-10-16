21204118a58ff3a1c71f774a8fe8e4e89befd026
Epiczd <nutball37@gmail.com>
2019-10-16 10:23:15 -0400

    Working on door script.

---
+++ Descent into Ere/Assets/Scripts/Scenes/DoorEnter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DoorEnter : MonoBehaviour {
+
+    [SerializeField] private string newLevel;
+
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("EnterDoor"))
+        {
+            SceneManager.LoadScene(newLevel);
+        }
+    }
+
+}
+
