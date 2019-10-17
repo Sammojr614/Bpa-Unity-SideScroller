4e27bafee7b0b640664885a90e2810bd3a59c9f8
Epiczd <nutball37@gmail.com>
2019-10-17 08:39:41 -0400

    Now the game only starts when the start button is clicked.

--- Descent into Ere/Assets/Scripts/OnClickEvents/StartClickSensor.cs
+++ Descent into Ere/Assets/Scripts/OnClickEvents/StartClickSensor.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartClickSensor : MonoBehaviour {
+public class StartClickSensor : MonoBehaviour
+{
 
-	public static bool clicked;
-	SpriteRenderer startButton;
-	void Start () {
-		startButton = gameObject.GetComponent<SpriteRenderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetMouseButtonDown(0)){
-			clicked = true;
-			startButton.enabled = false;
+    public static bool clicked;
+    SpriteRenderer startButton;
+    void Start()
+    {
+        startButton = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clicked = true;
+            startButton.enabled = false;
             Cursor.visible = false;
             SceneManager.LoadScene("MainHub");
-		}else{
-			if(Input.GetMouseButtonUp(0)){
-				clicked = false;
-			}
-		}
-	}
+        }
+        else
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                clicked = false;
+            }
+        }
+    }
 }
