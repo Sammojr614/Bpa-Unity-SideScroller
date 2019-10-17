789948b2b1fe1f115e50ad7d674d12c49f23e9a9
Sammojr614 <Sammojr614@gmail.com>
2019-10-17 08:26:17 -0400

    More data Handling

---
+++ Descent into Ere/Assets/Scripts/DataBase/LocationHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using System.IO;
+using Mono.Data.Sqlite;
+
+public class LocationHandler : MonoBehaviour {
+	public static string PlayerLocation;
+	
+	void Start () {
+		IDbConnection dbCon = new SqliteConnection(TitleData.connectionString);
+		dbCon.Open();
+		IDataReader DbReader;
+		IDbCommand cmdReader = dbCon.CreateCommand();
+		string readLine = "SELECT PlayerLocation FROM PlayerSaveData";
+		cmdReader.CommandText = readLine;
+		DbReader = cmdReader.ExecuteReader();
+		while(DbReader.Read()){
+			PlayerLocation = DbReader[0].ToString();
+		}
+		DbReader.Close();
+	}
+	
+	
+}
