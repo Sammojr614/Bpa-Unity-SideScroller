c15ed481d483ea76bf3cdc42840f0d7e9c7807b8
Epiczd <nutball37@gmail.com>
2019-10-17 08:27:33 -0400

    Added falling platform in PlayerDream, and disabled animator.

---
+++ Descent into Ere/Assets/Scripts/Enviornment/FallingPlatform.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+	
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag.Equals("Player"))
+        {
+            Invoke("DropPlatform", 0.5f);
+            Destroy(gameObject, 2f);
+        }
+    }
+
+    void DropPlatform()
+    {
+        rb.isKinematic = false;
+    }
+}
