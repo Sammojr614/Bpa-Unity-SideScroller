65c806b23896a523d363f97909e76b93edfee308
Epiczd <nutball37@gmail.com>
2019-10-21 10:40:19 -0400

    Added inventory menu
    Press tab to open/close, only in MainHub right now.

---
+++ Descent into Ere/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    public GameObject inventoryMenu;
+    public bool inventoryOpen = false;
+
+
+	void Start () {
+        inventoryMenu.SetActive(false);
+	}
+	
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (inventoryOpen)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                Time.timeScale = 0;
+                inventoryOpen = true;
+                Cursor.visible = true;
+                inventoryMenu.SetActive(true);
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        inventoryOpen = false;
+        Cursor.visible = false;
+        inventoryMenu.SetActive(false);
+    }
+}
