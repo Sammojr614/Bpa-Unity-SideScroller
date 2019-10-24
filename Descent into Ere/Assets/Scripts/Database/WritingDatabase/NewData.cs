feee581ec007398ddebc0764dbc768a1fd80b60a
Sammojr614 <Sammojr614@gmail.com>
2019-10-24 09:14:08 -0400

    Creating PlayerDream

--- Descent into Ere/Assets/Scripts/Database/WritingDatabase/NewData.cs
+++ Descent into Ere/Assets/Scripts/Database/WritingDatabase/NewData.cs
@@ -11,12 +11,19 @@ public class NewData : MonoBehaviour {
     DbManager dataManager = DbManager.Instance;
  void Start(){
 	 CreateData = gameObject.GetComponent<SpriteRenderer>();
-	 dataManager.RendererState(dataManager.connectionString, "SELECT*FROM PlayerSaveData", CreateData, false,true);
+	
  }
-	void OnMouseDown(){
+ void OnMouseEnter(){
+	  dataManager.RendererState(dataManager.connectionString, "SELECT*FROM PlayerSaveData", CreateData, false,true);
+ }
+ void OnMouseExit(){
+	  dataManager.RendererState(dataManager.connectionString, "SELECT*FROM PlayerSaveData", CreateData, false,true);
+ }
+ 	void OnMouseDown(){
 		if (Input.GetMouseButtonDown (0)) {
             dataManager.dbCommand(dataManager.connectionString, "CREATE TABLE PlayerSaveData(PlayerLocation TEXT,PlayerProgress TEXT,PlayerHealth INTEGER,PlayerLives INTEGER)");
             dataManager.dbCommand(dataManager.connectionString, "INSERT INTO PlayerSaveData(PlayerLocation,PlayerProgress,PlayerHealth,PlayerLives) VALUES('MainHub','0%','3','3')");
+			dataManager.loadSceneFromDB(dataManager.connectionString,"SELECT*FROM PlayerSaveData", LocateMainHub.PlayerLocation);
 			
 		}
 	}
