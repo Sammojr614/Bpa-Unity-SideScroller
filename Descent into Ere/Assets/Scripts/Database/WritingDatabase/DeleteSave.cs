8e768a0a89bc927c421521b2ca8ee02b59d3993f
Sammojr614 <Sammojr614@gmail.com>
2019-10-24 10:42:15 -0400

    Working On Saving Inventory

---
+++ Descent into Ere/Assets/Scripts/Database/WritingDatabase/DeleteSave.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteSave : MonoBehaviour {
+
+//This is For All Of the Db Scripts Used Here
+	DbManager dataManager = DbManager.Instance;
+	SpriteRenderer DeleteFile;
+	void Start(){
+		DeleteFile = gameObject.GetComponent<SpriteRenderer>();
+		dataManager.RendererState(dataManager.connectionString,"SELECT*FROM PlayerSaveData",DeleteFile, true,false);
+	}
+	void OnMouseDown(){
+		if(Input.GetMouseButtonDown(0)){
+			// As the Name of the Script Implies It Deletes the data
+			dataManager.dbCommand(dataManager.connectionString, "DROP TABLE PlayerSaveData");
+			dataManager.dbCommand(dataManager.connectionString, "DROP TABLE PlayerInventory");
+		}
+	}
+}
---
+++ Descent into Ere/Assets/Scripts/U.I/Inventory/collectItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectItem : MonoBehaviour {
+	public GameObject Item;
+	void Start(){
+		Item.GetComponent<GameObject>();
+		Item.SetActive(true);
+	}
+	
+	void OnTriggerEnter2D(Collider2D itemCollider){
+		if(itemCollider.CompareTag("Player")){
+			Inventory.NumberOfItems++;
+			Item.SetActive(false);
+			
+		}
+	}
+}
