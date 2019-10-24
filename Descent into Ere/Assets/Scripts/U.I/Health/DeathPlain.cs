c06bc9cbaa88fd2a96e32c4276a9953ede8df0f7
Sammojr614 <Sammojr614@gmail.com>
2019-10-24 09:57:40 -0400

    Working On Deathplain

--- Descent into Ere/Assets/Scripts/U.I/Health/DeathPlain.cs
+++ Descent into Ere/Assets/Scripts/U.I/Health/DeathPlain.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathPlain : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+void OnTriggerEnter(){
+	if(CompareTag("Player")){
+		PlayerHealth.health = 0;
 	}
 }
+}
+
+
