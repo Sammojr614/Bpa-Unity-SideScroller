109324e48a00ab4fd94da8fc5c6812e965a8b901
Epiczd <nutball37@gmail.com>
2019-10-25 09:48:58 -0400

    Potion can now only be used once.

--- Descent into Ere/Assets/Scripts/U.I/Inventory/Potion.cs
+++ Descent into Ere/Assets/Scripts/U.I/Inventory/Potion.cs
@@ -6,12 +6,15 @@ public class Potion : MonoBehaviour {
 
     public Animator health;
 
+    public GameObject potion;
+
     public void OnMouseDown()
     {
         if (Input.GetMouseButtonDown(0))
         {
             health.GetComponent<PlayerHealth>().Heal();
             Inventory.NumberOfItems--;
+            potion.SetActive(false);
         }
     }
 }
