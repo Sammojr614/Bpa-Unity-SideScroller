7bba535cf47d2fab4cf273a36f49ed6672f2e08a
Sammojr614 <Sammojr614@gmail.com>
2019-10-25 10:48:34 -0400

    I have No Idea

--- Descent into Ere/Assets/Scripts/Player/FlipSprites.cs
+++ Descent into Ere/Assets/Scripts/Player/FlipSprites.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FlipSprites : MonoBehaviour {
- SpriteRenderer Player;
+	
+	
 	// Use this for initialization
 	void Start () {
-		Player = gameObject.GetComponent<SpriteRenderer>();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButtonDown("MoveLeft")){
-			Player.flipY = true;
-		}else{
-			if(Input.GetButtonDown("MoveRight")){
-				Player.flipY = false;
-			}
-		}
+	
 	}
 }
