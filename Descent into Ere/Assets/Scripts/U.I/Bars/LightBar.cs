a67a0450b0d19e1989f2398a9e662d78be07eb94
Sammojr614 <Sammojr614@gmail.com>
2019-10-29 09:57:44 -0400

    Lightbarstuff

--- Descent into Ere/Assets/Scripts/U.I/Bars/LightBar.cs
+++ Descent into Ere/Assets/Scripts/U.I/Bars/LightBar.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 public class LightBar : MonoBehaviour {
 
+Animator LighBarAnimator;
 public static float SprintLeft;
 public static  float MPdrain;
 void Start(){
      SprintLeft = 20;
+     LighBarAnimator = gameObject.GetComponent<Animator>();
+      LighBarAnimator.SetFloat("Sprinting", 0.5f);
 }
 void Update(){
     if(Input.GetButton("Sprint")){
         SprintLeft--;
+        LighBarAnimator.SetFloat("Sprinting", 0.0f);
     }else{
         if(Input.GetButtonUp("Sprint")){
-            while(SprintLeft < 100){
+            while(SprintLeft < 20){
                 SprintLeft++;
+                LighBarAnimator.SetFloat("Sprinting", 1.0f);
             }
+           
         }
     }
     }
