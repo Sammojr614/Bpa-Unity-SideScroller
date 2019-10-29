4e3347f67c96e73075df0d19707bd62d3724cd8f
Sammojr614 <Sammojr614@gmail.com>
2019-10-29 09:20:55 -0400

    Fixing LightBar

--- Descent into Ere/Assets/Scripts/U.I/Bars/LightBar.cs
+++ Descent into Ere/Assets/Scripts/U.I/Bars/LightBar.cs
@@ -3,64 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 public class LightBar : MonoBehaviour {
 
-    Animator anim;
-    private float x;
-    private float y;
-    private float sprint;
-    private GameObject lightBar;
-
-    private Move2D controller;
-
-    public float slowSpeed = 3f;
-
-    //Total amount of stamina
-    public float amount = 100f;
-
-    //Amount of stamina drained
-    public float drainAmount = 5f;
-    //Amount of stamina regenerated
-    public float regenAmount = 5f;
-	// Use this for initialization
-	void Start () {
-        controller = GameObject.FindObjectOfType<Move2D>();
-        lightBar = GameObject.FindGameObjectWithTag("Light Bar");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //Buttons for sprinting
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        bool isRunningRight = Input.GetKey(KeyCode.RightShift);
-
-        /* If the player is sprinting
-         * The player will lose stamina
-         */
-        if (isRunning || isRunningRight)
-        {
-            amount -= Time.deltaTime * drainAmount;
-        }
-        else
-        {
-            /* If the player has full stamina
-             * They will not regenerate anymore
-             */
-            if(amount >= 100f)
-            {
-                return;
+public static float SprintLeft;
+public static  float MPdrain;
+void Start(){
+     SprintLeft = 20;
+}
+void Update(){
+    if(Input.GetButton("Sprint")){
+        SprintLeft--;
+    }else{
+        if(Input.GetButtonUp("Sprint")){
+            while(SprintLeft < 100){
+                SprintLeft++;
             }
-            /* If the player is no longer sprinting
-             * The player will regenrate stamina
-             */
-            amount += Time.deltaTime * regenAmount;
         }
-
-        
-        if(amount == 0f)
-        {
-            
-        }
-        
-        
-
+    }
     }
 }
+
