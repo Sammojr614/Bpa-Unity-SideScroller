870dbf2edb9aed75387e1f6e6314073a8208b678
Sam <sammojr614@gmail.com>
2019-10-30 19:29:41 -0400

    Working on lightbar Schinanigans

---
+++ Descent into Ere/Assets/Scripts/U.I/Bars/LightSideManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSideManager : MonoBehaviour {
+	
+	Vector3 RestoreVector = new Vector3(10f,0f,0f);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Sprint")){
+			for(var i = 0; i < 3; i++){
+				transform.localScale -= RestoreVector;
+
+			}
+			
+			
+
+		}else{
+			if(Input.GetButtonUp("Sprint")){
+				while(LightBar.SprintLeft < 20){
+					for (var i = 0; i < 3; i++) {
+						transform.localScale += RestoreVector;
+					}
+
+			}
+
+		}
+		}
+	}
+}
