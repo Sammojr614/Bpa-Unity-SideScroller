a69bd552fbf0445b5bf23e26e1edc72a5d69333f
Epiczd <nutball37@gmail.com>
2019-10-31 08:24:17 -0400

    Still working on lucid, fixed the camera issue.

---
+++ Descent into Ere/Assets/Scripts/Level/Key.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+    public GameObject key;
+    public GameObject Obsticle;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            DestroyObject(Obsticle);
+            key.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            DestroyObject(Obsticle);
+            key.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
+}
