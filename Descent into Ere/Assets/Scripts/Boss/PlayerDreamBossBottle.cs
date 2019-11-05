f6f5158128135fbc8d6696902b831dde8fce8288
Sammojr614 <Sammojr614@gmail.com>
2019-11-05 10:50:23 -0500

    Scene Work

---
+++ Descent into Ere/Assets/Scripts/Boss/PlayerDreamBossBottle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDreamBossBottle : MonoBehaviour {
+
+	public GameObject BossBottle;
+	public GameObject Boss;
+	public static bool BeginBoss;
+	void Start () {
+		Boss.SetActive(false);
+	}
+	void OnTriggerEnter2D(Collider2D collider){
+		if(collider.CompareTag("Player")){
+			if(Input.GetButton("EnterDoor")){
+				BeginBoss = true;
+			}
+		}
+	}
+}
--- Descent into Ere/Assets/Scripts/Scenes/BossDoor.cs
+++ Descent into Ere/Assets/Scripts/Scenes/BossDoor.cs
@@ -8,7 +8,7 @@ public GameObject BossDoorOpen;
 	void OnTriggerStay2D(Collider2D collision){
 		if (collision.CompareTag ("Player")) {
 			if (Input.GetButton ("EnterDoor") && Inventory.haveBossKey == true) {
-				SceneManager.LoadScene ("PlayerDreamPt2");
+				SceneManager.LoadScene ("PlayerDreamBoss");
 				Inventory.haveBossKey = false;
 				if (Inventory.NumberOfItems < 0) {
 					Inventory.NumberOfItems--;
