6d675b4624f6164e0f2e9d9d620f7f76cca74da6
Epiczd <nutball37@gmail.com>
2019-11-06 09:04:59 -0500

    Attack Button is now f due to mouse not working after closing the inventory.

--- Descent into Ere/Assets/Scripts/Combat/Sword.cs
+++ Descent into Ere/Assets/Scripts/Combat/Sword.cs
@@ -16,7 +16,7 @@ public class Sword : MonoBehaviour {
 	void Update () {
 		if(timeBtwAttack <= 0)
         {
-            if (Input.GetMouseButton(0))
+            if (Input.GetButton("SwordAttack"))
             {
                 Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
                 for (int i = 0; i < enemiesToDamage.Length; i++)
