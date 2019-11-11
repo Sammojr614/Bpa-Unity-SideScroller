345a7fe97232d97a0cd58ef13b83f49163258f52
Sammojr614 <Sammojr614@gmail.com>
2019-11-11 10:46:15 -0500

    Working on Tutoral

---
+++ Descent into Ere/Assets/Scripts/Tutoral/Sign.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sign : MonoBehaviour {
+	public GameObject TurtoralText;
+	public static bool playerInBox;
+	void Start(){
+		TurtoralText.SetActive(false);
+	}
+void OnTriggerEnter2D(Collider2D collider){
+	playerInBox = true;
+	if(Input.GetButton("EnterDoor")){
+		TurtoralText.SetActive(true);
+
+	}
+}
+void OnTriggerStay2D(Collider2D collider){
+	playerInBox = true;
+}
+void OnTriggerExit2D(Collider2D collider){
+	TurtoralText.SetActive(false);
+	playerInBox = true;
+
+}
+}
---
+++ Descent into Ere/Assets/Scripts/Tutoral/TextandTextbox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class TextandTextbox : MonoBehaviour {
+	public GameObject[] letters;
+	void Update(){
+		if(Sign.playerInBox == true){
+			foreach(var indexNumber in letters){
+				int IndexValue;
+				for(IndexValue = 0; IndexValue < Convert.ToInt32(indexNumber)-1; IndexValue++){
+					letters[IndexValue].SetActive(true);
+				}
+			}
+	}
+	}
+
+}
