c04a244dbbeb2a7f004961f18f9373675e9a1754
Epiczd <nutball37@gmail.com>
2019-11-14 10:11:21 -0500

    Created Switch Script

---
+++ Descent into Ere/Assets/Scripts/Enviornment/Level/Switch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+    public GameObject TrapDoor;
+    public GameObject Button;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Button.SetActive(false);
+            DestroyObject(TrapDoor);
+        }
+    }
+}
