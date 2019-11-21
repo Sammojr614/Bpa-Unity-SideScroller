22c5df59d63bf80f711bf80b2fb64b5066e7d4c5
Epiczd <nutball37@gmail.com>
2019-11-21 09:26:06 -0500

    Made some changes to the Boss Door script.
    You now set the boss scene in unity, as before it would only take you to player dream boss.

--- Descent into Ere/Assets/Scripts/Enviornment/Scenes/BossDoor.cs
+++ Descent into Ere/Assets/Scripts/Enviornment/Scenes/BossDoor.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BossDoor : MonoBehaviour {
+
+//BossDoor
 public GameObject BossDoorOpen;
-	void OnTriggerStay2D(Collider2D collision){
-		if (collision.CompareTag ("Player")) {
-			if (Input.GetButton ("EnterDoor") && Inventory.haveBossKey == true) {
-				SceneManager.LoadScene ("PlayerDreamBoss");
-				Inventory.haveBossKey = false;
-				if (Inventory.NumberOfItems < 0) {
-					Inventory.NumberOfItems--;
+//Scene where the boss is
+public string BossScene;
+
+/* While the player is colliding with
+ * The boss door's trigger, and has the boss key
+ * The player will enter the boss scene
+ */
+
+void OnTriggerStay2D(Collider2D collision){
+    if (collision.CompareTag ("Player")) {
+        if (Input.GetButton ("EnterDoor") && Inventory.haveBossKey == true) {
+            SceneManager.LoadScene (BossScene);
+            Inventory.haveBossKey = false;
+            if (Inventory.NumberOfItems < 0) {
+                    Inventory.NumberOfItems--;
 				}
 			}
 		}
