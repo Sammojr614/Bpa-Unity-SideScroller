c2e8402fdc038cb8a7c5c56e7d14d5c9d9fdbc9a
Sammojr614 <Sammojr614@gmail.com>
2019-12-02 09:02:48 -0500

    No More MoonWalk

---
+++ Descent into Ere/Assets/Scripts/Enviornment/Player/Visuals/Flipper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour {
+	
+	public SpriteRenderer PlayerRenderer;
+	Transform PlayerTrans;
+	void Start(){
+		PlayerTrans = gameObject.GetComponent<Transform>();
+	}
+	void Update () {
+		if(Input.GetButton("MoveLeft")){
+			PlayerRenderer.flipX = true;
+		}else if(Input.GetButton("MoveRight")){
+			PlayerRenderer.flipX = false;
+		}
+	}
+}
---
+++ Descent into Ere/Assets/Scripts/Enviornment/Player/Visuals/LeftMovementAnimations.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftMovementAnimations : MonoBehaviour {
+	public Animator PlayerAnimator;
+	public static bool FaceingLeft;
+	void Update () {
+		if(Input.GetButton("MoveLeft")){
+			FaceingLeft = true;
+			PlayerAnimator.SetBool("isFacingLeft", FaceingLeft);
+		}else if(Input.GetButton("MoveRight")){
+			FaceingLeft = false;
+			PlayerAnimator.SetBool("isFacingLeft", FaceingLeft);
+		}
+	}
+}
