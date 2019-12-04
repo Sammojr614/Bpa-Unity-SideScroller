6e07a3840e8cb8cc7b6bb5d490e99fe15a838fed
Sammojr614 <Sammojr614@gmail.com>
2019-12-04 09:11:37 -0500

    Removed Anima2D and Resolved 1 merge

--- Descent into Ere/Assets/Scripts/Shop/MoveScenesWithButton2D.cs
+++ Descent into Ere/Assets/Scripts/Shop/MoveScenesWithButton2D.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MoveScenesWithButton2D : MonoBehaviour {
-	public string SceneName;
 	DbManager dataManager = DbManager.Instance;
+	public string SceneName;
+	public string PrevScene;
+	
+	void Start(){
+	dataManager.dbFindStringData(dataManager.connectionString, "SELECT*FROM PlayerSaveData", PrevScene, 0 );
+	}
 	void OnMouseDown(){
 		if(Input.GetMouseButtonDown(0)){
-			SceneManager.LoadScene(SceneName);
+			SceneManager.LoadScene(PrevScene);
 			string insertThis = string.Format("UPDATE ShopStock SET NumberOfItems={0} WHERE Items='Potion'", ShopTable.NumberOfItems);
 			Debug.Log(ShopTable.NumberOfItems);
 			dataManager.dbCommand(dataManager.connectionString,insertThis);
