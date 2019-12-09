d093570eaa7bb5af2ff71d75894409ebd059954e
Epiczd <nutball37@gmail.com>
2019-12-09 13:33:49 -0500

    Added WeaponActive Script

---
+++ Descent Into Ere/Assets/Scripts/Enviornment/Level/Player/Combat/WeaponActive.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponActive : MonoBehaviour {
+
+    //Knife used by the player
+    public GameObject ButterKnife;
+    //Checks if knife is drawn
+    public bool KnifeDrawn;
+
+    //On Start, the knife is equipped
+	void Start () {
+        ButterKnife.SetActive(true);
+        KnifeDrawn = true;
+    }
+	
+	/* If the player presses F, 
+     * and the knife is drawn,
+     * the player will put away the knife,
+     * On the contrary, if the player presses F,
+     * and the knife is away,
+     * the player will draw the knife
+     */
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F) && KnifeDrawn == true)
+        {
+            ButterKnife.SetActive(false);
+            KnifeDrawn = false;
+        }
+        else if(Input.GetKeyDown(KeyCode.F) && KnifeDrawn == false)
+        {
+            ButterKnife.SetActive(true);
+            KnifeDrawn = true;
+        }
+	}
+}
