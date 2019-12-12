a679066e763c6c49ac9303ea902238831e4f0a08
Sammojr614 <Sammojr614@gmail.com>
2019-12-12 09:15:59 -0500

    Able to Get to Level3 From MainHub now

---
+++ Descent Into Ere/Assets/Animations/CheckBoxes/CheckBox3.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckBox3 : MonoBehaviour {
+	public GameObject Check;
+	GameObject EmptyCheck;
+void Start(){
+	EmptyCheck = gameObject.GetComponent<GameObject>();
+}
+	void Update(){
+		if(LevelCompletion.LevelComplete == 3){
+			Check.SetActive(true);
+			EmptyCheck.SetActive(false);
+		}else{
+			Check.SetActive(false);
+			EmptyCheck.SetActive(true);
+		}
+	}
+}
