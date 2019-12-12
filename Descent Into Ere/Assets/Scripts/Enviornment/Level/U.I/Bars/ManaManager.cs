8c8e05da2b66202e7ca4fe3e90ebcd205ebc11a3
Sammojr614 <Sammojr614@gmail.com>
2019-12-12 10:00:35 -0500

    Fixed Mana Stuff

--- Descent Into Ere/Assets/Scripts/Enviornment/Level/U.I/Bars/ManaManager.cs
+++ Descent Into Ere/Assets/Scripts/Enviornment/Level/U.I/Bars/ManaManager.cs
@@ -3,33 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ManaManager : MonoBehaviour {
-	public GameObject[] Charges;
-	int chargesUsed;
-	void Start(){
-		chargesUsed = 0;
-	}
+public GameObject[] Charges;
+public static int chargesUsed;
+
 	void Update(){
-		if(chargesUsed == 0){
-			foreach(var charge in Charges){
-				for(var i = 0; i < Charges.Length; i++){
-				Charges[i].SetActive(true);
-				}
-			}
-		}
 		if(Input.GetButtonDown("ManaAttack")){
-			if(chargesUsed <= 2){
-				Charges[chargesUsed].SetActive(false);
-				chargesUsed++;
+			chargesUsed++;
+			if(chargesUsed == 4){
+					Charges[3].SetActive(false);
 			}else if(chargesUsed == 3){
-				while(chargesUsed >= -1){
-					Charges[chargesUsed].SetActive(true);
-					chargesUsed--;
-					if(chargesUsed == 0){
-						chargesUsed = 0;
-						break;
+						Charges[2].SetActive(false);
+				}else if(chargesUsed == 2){
+					Charges[1].SetActive(false);
+				}else if(chargesUsed == 1){
+						Charges[0].SetActive(false);
 					}
+			if(chargesUsed >= 4){
+				while(chargesUsed >= 0){
+					if(chargesUsed == 3){
+						Charges[3].SetActive(true);
+					}else if(chargesUsed == 2){
+						Charges[2].SetActive(true);
+					}else if(chargesUsed == 1){
+						Charges[1].SetActive(true);
+					}else if(chargesUsed == 0){
+						Charges[0].SetActive(true);
+					}
+					chargesUsed--;
 				}
 			}
+			
 		}
 	}
 }
