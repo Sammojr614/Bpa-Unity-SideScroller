8bafecbeb50dc1f04850235e27749034525ee651
Epiczd <nutball37@gmail.com>
2019-12-12 22:56:00 -0500

    Working on ManaAttack Script

---
+++ Descent Into Ere/Assets/Scripts/Enviornment/Level/Player/Combat/MagicAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicAttack : MonoBehaviour {
+
+    public GameObject magicBall;
+
+    public static float timeBtwAttack = 0f;
+
+    public static bool magicDamage;
+
+    public static bool canAttack;
+
+    private Vector3 MovingDirection = Vector3.right;
+
+	void Start () {
+        magicDamage = false;
+        canAttack = true;
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(timeBtwAttack <= 0f)
+        {
+            if(Input.GetButtonDown("ManaAttack") && canAttack == true)
+            {
+                
+            }
+        }
+    }
+}
