898c96c3547aec514e37824f78f641524e775ad9
Sammmojr <sammojr614@gmail.com>
2019-12-17 09:49:38 -0500

    addded Stuff to the Credit Scene

---
+++ Descent Into Ere/Assets/Scripts/Random/RevealOnMouseOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevealOnMouseOver : MonoBehaviour {
+    public GameObject textToReveal;
+
+   void Start()
+    {
+        textToReveal.SetActive(false);
+    }
+
+    void OnMouseEnter()
+    {
+        textToReveal.SetActive(true);
+    }
+        void OnMouseExit()
+    {
+        textToReveal.SetActive(false);
+    }
+}
