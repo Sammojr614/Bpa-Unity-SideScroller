0daf5c88dd607f209f6353be00cfed1488fadbec
Epiczd <nutball37@gmail.com>
2019-12-19 10:02:48 -0500

    Progress on SpikeActive

---
+++ Descent Into Ere/Assets/Scripts/Enviornment/Level/PlatForming/SpikeActive.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeActive : MonoBehaviour
+{
+
+    public GameObject spike;
+
+    //On start, the spike is hidden
+    void Start()
+    {
+        spike.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            spike.SetActive(true);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            spike.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        spike.SetActive(false);
+    }
+}
