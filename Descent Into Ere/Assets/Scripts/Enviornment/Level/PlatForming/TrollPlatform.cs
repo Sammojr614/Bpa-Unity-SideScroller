cb77d2f4638ab0c7335e3e66c0743894f63eddbb
Sammmojr <sammojr614@gmail.com>
2019-12-19 10:03:54 -0500

    Pt 3 Lower Work

---
+++ Descent Into Ere/Assets/Scripts/Enviornment/Level/PlatForming/TrollPlatform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrollPlatform : MonoBehaviour
+{
+    public GameObject TrollPlatformGameObject;
+ void OnTriggerEnter2D(Collider2D collider){
+     if(collider.CompareTag("Player")){
+         TrollPlatformGameObject.SetActive(false);
+     }
+ }
+
+}
