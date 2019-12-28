186e85b7df4199fd0d80bea105e35d885ee9e70d
Epiczd <nutball37@gmail.com>
2019-12-28 18:07:15 -0500

    Laser Script Added

---
+++ Descent Into Ere/Assets/Scripts/Enviornment/Level/Other/Laser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    //SpriteRenderer for the laser
+    public SpriteRenderer laser;
+
+    //On start, the laser is disabled
+    void Start()
+    {
+        laser.enabled = false;
+    }
+
+    /* If the player steps on the trigger platform,
+     * a deadly laser is activated, and serves as an obsticle,
+     * for the player to avoid.
+     */
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            laser.enabled = true;
+        }
+    }
+}
