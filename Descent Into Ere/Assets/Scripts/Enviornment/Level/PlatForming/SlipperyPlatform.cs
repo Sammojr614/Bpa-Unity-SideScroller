b43922cef1d4d039566c307059bd31b02c7ea6d5
Epiczd <nutball37@gmail.com>
2019-12-28 21:30:49 -0500

    Slippery Platform Script Completed

--- Descent Into Ere/Assets/Scripts/Enviornment/Level/PlatForming/SlipperyPlatform.cs
+++ Descent Into Ere/Assets/Scripts/Enviornment/Level/PlatForming/SlipperyPlatform.cs
@@ -4,14 +4,63 @@ using UnityEngine;
 
 public class SlipperyPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject player;
+
+    public bool facingLeft;
+
+    private Vector3 MovingDirection;
+
     void Start()
     {
-        
+        facingLeft = false;
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        if (Input.GetButton("MoveRight"))
+        {
+            facingLeft = false;
+        }
+        else if (Input.GetButton("MovingLeft"))
+        {
+            facingLeft = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if(facingLeft == true)
+            {
+                MovingDirection = Vector3.left;
+            }
+            else if(facingLeft == false)
+            {
+                MovingDirection = Vector3.right;
+            }
+            player.transform.Translate(MovingDirection * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (facingLeft == true)
+            {
+                MovingDirection = Vector3.left;
+            }
+            else if (facingLeft == false)
+            {
+                MovingDirection = Vector3.right;
+            }
+            player.transform.Translate(MovingDirection * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
     {
         
     }
