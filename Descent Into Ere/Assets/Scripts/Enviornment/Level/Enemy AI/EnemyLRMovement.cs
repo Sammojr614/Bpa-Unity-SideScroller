77da2f41d8b434067371d2029336add343c20fb4
Epiczd <nutball37@gmail.com>
2020-01-04 15:28:30 -0500

    Made changes to EnemyLRMovement Script

--- Descent Into Ere/Assets/Scripts/Enviornment/Level/Enemy AI/EnemyLRMovement.cs
+++ Descent Into Ere/Assets/Scripts/Enviornment/Level/Enemy AI/EnemyLRMovement.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class EnemyLRMovement : MonoBehaviour
 {
+    //Moving Direction
     private Vector3 MovingDirection = Vector3.left;
 
+    //Minimum x position for the enemy
+    public float xMin;
+    //Maximum x position for the enemy
+    public float xMax;
 
-    // Update is called once per frame
+
+    // Updates the enemies movement
     void Update()
     {
         UpdateMovement();
     }
 
+    /* If the enemy's x position is greater than the max,
+     * the enemy will move left, and the sprite renderer will flip.
+     * Conversly, if the enemy's x position is less than the min,
+     * the enemy will move right, and the sprite renderer will flip back to the original
+     */
     void UpdateMovement(){
-        if (this.transform.position.x > 2.4f) {
+        if (this.transform.position.x > xMax) {
             MovingDirection = Vector3.left;
             gameObject.GetComponent<SpriteRenderer>().flipX = true;
-        } else if (this.transform.position.x < -2f){
+        } else if (this.transform.position.x < xMin){
             MovingDirection = Vector3.right;
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
