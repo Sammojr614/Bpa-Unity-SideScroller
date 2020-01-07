15c0f087916fa0f5d7c4e5d9f90b861bec3cb176
Sammojr <sammojr614@gmail.com>
2020-01-06 21:51:17 -0500

    Game Over and Title Have New Visuals

---
+++ Descent Into Ere/Assets/Scripts/Random/GameOverStuff.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverStuff : MonoBehaviour
+{
+    public GameObject ContinueButton;
+    public GameObject QuitButton;
+     void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
