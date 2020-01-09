cbd311810e77f23c0c458b9962af45b3e1cfdb76
Sammmojr <sammojr614@gmail.com>
2020-01-09 09:59:52 -0500

    U.I Changes

---
+++ Descent Into Ere/Assets/PotionButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PotionButton : MonoBehaviour
+{
+   public Text CantUseText;
+   public SpriteRenderer PotionButtonGameObject;
+   public Text UseButtonText;
+  void Update(){
+      if(Input.GetMouseButtonDown(0)){
+          if(Inventory.NumberOfPotions >= 0){
+              CantUseText.enabled = true;
+              PotionButtonGameObject.enabled = false;
+              UseButtonText.enabled = false;
+          }
+      }
+      if(Input.GetMouseButtonUp(0)){
+          CantUseText.enabled = false;
+          PotionButtonGameObject.enabled = true;
+          UseButtonText.enabled = true;
+      }
+  }
+}
