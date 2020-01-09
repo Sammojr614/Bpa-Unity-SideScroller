1baaf19831cbe11078c8720a40685bb411290924
Sammmojr <sammojr614@gmail.com>
2020-01-09 10:08:08 -0500

    Added new UI to all Scenes

--- Descent into Ere/Assets/Scripts/Enviornment/Level/U.I/Inventory/Potion.cs
+++ Descent into Ere/Assets/Scripts/Enviornment/Level/U.I/Inventory/Potion.cs
@@ -1,30 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Potion : MonoBehaviour {
 
     public Animator health;
 
     public GameObject useButton;
-    public Text CantUseText;
-    void Start(){
-        CantUseText.enabled = false;
-    }
+    
+   
     void Update(){
-        if (Input.GetMouseButtonDown(0) && Inventory.NumberOfPotions <= 0) {
-                CantUseText.enabled = true;
-                useButton.SetActive(false);
-            }
-            if(Input.GetMouseButtonUp(0)){
-                useButton.SetActive(true);
-                CantUseText.enabled = false;
-            }
-    }
-    public void OnMouseDown()
-    {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0) && Inventory.NumberOfPotions > 0)
         {
 				PlayerHealth.Heal (health);
 				Inventory.NumberOfItems--;
@@ -32,5 +18,4 @@ public class Potion : MonoBehaviour {
                 
         }
     }
-
-}
+}   
