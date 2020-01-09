0c9782cae22bc057335c8980576c74bf593ad96e
Sammmojr <sammojr614@gmail.com>
2020-01-09 10:46:44 -0500

    more UI Stuff

---
+++ Descent Into Ere/Assets/Scripts/Visuals/DevSceneButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevSceneButtons : MonoBehaviour
+{
+    public GameObject SmallButton;
+    public GameObject BigScreen;
+    public GameObject SmallinButton;
+    void Start(){
+        SmallButton.SetActive(true);
+        BigScreen.SetActive(false);
+        SmallButton.SetActive(false);
+    }
+    void OnMouseDown(){
+        if(Input.GetMouseButtonDown(0)){
+            BigScreen.SetActive(true);
+            SmallButton.SetActive(false);
+            SmallinButton.SetActive(true);
+        }
+    }
+}
