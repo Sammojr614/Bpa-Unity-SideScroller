de66366386e3deae5f2b007976557c2ab4c7a689
Epiczd <nutball37@gmail.com>
2020-01-09 10:25:03 -0500

    Commented ManaManager Script

--- Descent Into Ere/Assets/Scripts/Enviornment/Level/U.I/Bars/ManaManager.cs
+++ Descent Into Ere/Assets/Scripts/Enviornment/Level/U.I/Bars/ManaManager.cs
@@ -21,6 +21,7 @@ public class ManaManager : MonoBehaviour {
     //Total amount of mana the player can use
     private float totalMana = 10f;
 
+    //On start, totalMana = 10f
     void Start()
     {
         totalMana = 10f;
@@ -29,6 +30,9 @@ public class ManaManager : MonoBehaviour {
     void Update()
     {
         Debug.Log(totalMana);
+        /* When the player attacks with the magic ball,
+         * they will lose a quarter of the totalMana
+         */
         if (Input.GetButtonDown("ManaAttack"))
         {
             if(totalMana != 0f && MagicAttack.canAttack == true)
@@ -38,6 +42,10 @@ public class ManaManager : MonoBehaviour {
         }
         else
         {
+            /* If the player has less than 10 total mana,
+             * and is not attacking, they will begin to regenerate mana,
+             * and once they reach max stamina, it will stop regenerating
+             */            
             if (totalMana < 10f)
             {
                 totalMana += regenTime * Time.deltaTime * 5;
@@ -48,6 +56,11 @@ public class ManaManager : MonoBehaviour {
             }
         }
 
+        /* Every 2.5f of mana the player loses,
+         * equates to one charge on the mana bar being disabled.
+         * Once the player regenerates the stamina,
+         * the charge will be reenabled
+         */
         if (totalMana <= 7.5f)
         {
             Charge1.SetActive(false);
@@ -75,6 +88,13 @@ public class ManaManager : MonoBehaviour {
             Charge3.SetActive(true);
         }
 
+        /* If the player reaches zero stamina,
+         * the final charge of the stamina bar is disabled, 
+         * and they can no longer attack with stamina.
+         * If they regenerate the stamina,
+         * the charge will be reenabled, 
+         * and they will be allowed to use the mana attack again
+         */        
         if (totalMana <= 0f)
         {
             Charge4.SetActive(false);
@@ -86,46 +106,5 @@ public class ManaManager : MonoBehaviour {
             MagicAttack.canAttack = true;
         }
     }
-
-    /*
-	void Update(){
-		if(Input.GetButtonDown("ManaAttack")){
-			chargesUsed++;
-			if(chargesUsed == 4){
-					Charges[3].SetActive(false);
-			}else if(chargesUsed == 3){
-						Charges[2].SetActive(false);
-				}else if(chargesUsed == 2){
-					Charges[1].SetActive(false);
-				}else if(chargesUsed == 1){
-						Charges[0].SetActive(false);
-					}
-			if(chargesUsed >= 4){
-				while(chargesUsed >= 0){
-					if(chargesUsed == 3){
-						Charges[3].SetActive(true);
-					}else if(chargesUsed == 2){
-						Charges[2].SetActive(true);
-					}else if(chargesUsed == 1){
-						Charges[1].SetActive(true);
-					}else if(chargesUsed == 0){
-						Charges[0].SetActive(true);
-					}
-					chargesUsed--;
-				}
-			}
-
-            if(chargesUsed == 4)
-            {
-                MagicAttack.canAttack = false;
-            }
-            else
-            {
-                MagicAttack.canAttack = true;
-            }
-
-        }
-	}
-	*/
 }
 
