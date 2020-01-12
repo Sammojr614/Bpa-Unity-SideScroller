5754301d882346aad8fc6861c5a1949233218df4
Epiczd <nutball37@gmail.com>
2020-01-12 18:11:19 -0500

    More Background Script Stuff

--- Descent Into Ere/Assets/Scripts/Enviornment/Level/Other/BackgroundColor.cs
+++ Descent Into Ere/Assets/Scripts/Enviornment/Level/Other/BackgroundColor.cs
@@ -15,12 +15,13 @@ public class BackgroundColor : MonoBehaviour
     void Start()
     {
         changeTime = 0f;
+        background.GetComponent<Renderer>().material.color = Color.white;
     }
 
     // Update is called once per frame
     void Update()
     {
-        changeTime += Time.deltaTime;
+        changeTime += Time.deltaTime * 0.3f;
 
         if(changeTime >= 1f)
         {
@@ -36,5 +37,41 @@ public class BackgroundColor : MonoBehaviour
         {
             background.GetComponent<Renderer>().material.color = Color.red;
         }
+
+        if(changeTime >= 0.3f)
+        {
+            background.GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        if(changeTime >= 0.4f)
+        {
+            background.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+
+        if(changeTime >= 0.5f)
+        {
+            background.GetComponent<Renderer>().material.color = Color.cyan;
+        }
+
+        if(changeTime >= 0.6f)
+        {
+            background.GetComponent<Renderer>().material.color = Color.grey;
+        }
+
+        if(changeTime >= 0.7f)
+        {
+            background.GetComponent<Renderer>().material.color = Color.grey;
+        }
+
+        if(changeTime >= 0.8f)
+        {
+            background.GetComponent<Renderer>().material.color = Color.magenta;
+        }
+
+        if(changeTime >= 0.9f)
+        {
+            background.GetComponent<Renderer>().material.color = Color.black;
+        }
+
     }
 }
