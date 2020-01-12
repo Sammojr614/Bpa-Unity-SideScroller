c345c1d4a81610fd3a55bab3c628cd8c5ec85503
Sammojr <sammojr614@gmail.com>
2020-01-12 16:16:44 -0500

    Worked on The inventory

--- Descent Into Ere/Assets/Scripts/Enviornment/Level/U.I/Inventory/PotionButton.cs
+++ Descent Into Ere/Assets/Scripts/Enviornment/Level/U.I/Inventory/PotionButton.cs
@@ -8,21 +8,25 @@ public class PotionButton : MonoBehaviour
    public Text CantUseText;
    public SpriteRenderer PotionButtonGameObject;
    public Text UseButtonText;
-  void Update(){
-      if(Inventory.inventoryOpen == false){
-          CantUseText.enabled = false;
-      }
-      if(Input.GetMouseButtonDown(0)){
-          if(Inventory.NumberOfPotions >= 0){
-              CantUseText.enabled = true;
-              PotionButtonGameObject.enabled = false;
-              UseButtonText.enabled = false;
-          }
-      }
-      if(Input.GetMouseButtonUp(0)){
-          CantUseText.enabled = false;
-          PotionButtonGameObject.enabled = true;
-          UseButtonText.enabled = true;
-      }
+    void Update() {
+        if (Inventory.inventoryOpen == false) {
+            CantUseText.enabled = false;
+        }
+        if (Input.GetMouseButtonDown(0)) {
+            if (Inventory.NumberOfPotions >= 0) {
+                CantUseText.enabled = true;
+                PotionButtonGameObject.enabled = false;
+                UseButtonText.enabled = false;
+            }
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            CantUseText.enabled = false;
+            PotionButtonGameObject.enabled = true;
+            UseButtonText.enabled = true;
+        }
+        if (Inventory.inventoryOpen == false)
+        {
+            CantUseText.enabled = false;
+        }
   }
 }
