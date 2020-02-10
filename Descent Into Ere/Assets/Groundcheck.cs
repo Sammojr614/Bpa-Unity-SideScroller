f3a3aa191ce3cc0a16c9ecfe657084af6c09764c
Epiczd <nutball37@gmail.com>
2020-02-10 10:41:28 -0500

    Testing

---
+++ Descent Into Ere/Assets/Groundcheck.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Groundcheck : MonoBehaviour
+{
+    //If the player is on the ground, isGrounded = true
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            PlayerMovement.isGrounded = true;
+        }
+    }
+
+    //If the player is NOT on the ground, isGrounded = false
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            PlayerMovement.isGrounded = false;
+        }
+    }
+}
---
+++ Descent Into Ere/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Speed the player moves
+    private float moveSpeed = 5f;
+
+    //Checks if the player is grounded
+    public static bool isGrounded;
+
+    //Player moves by using wsad or arrow keys
+    void Update()
+    {
+        Debug.Log(isGrounded);
+
+        Jump();
+        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += Movement * Time.deltaTime * moveSpeed;
+    }
+
+    /* If the player presses space while grounded,
+     * they will jump
+     */
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(0f, 8f, 0f), ForceMode2D.Impulse);
+        }
+    }
+}
