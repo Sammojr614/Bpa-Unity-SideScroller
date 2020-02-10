79dd1dc6a22d99be378e2c91bcd3ff0e5a195479
Sammojr <sammojr614@gmail.com>
2020-02-10 09:49:28 -0500

    yote all Scripts

---
+++ Descent Into Ere/Assets/Scripts/MovingScenes/MoveScenes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MoveScenes : MonoBehaviour
+{
+    public string SceneName;
+    public Button MoveSceneButton;
+    private void Start()
+    {
+        MoveSceneButton.onClick.AddListener(OnClickEvent);
+    }
+    void OnClickEvent()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+}
