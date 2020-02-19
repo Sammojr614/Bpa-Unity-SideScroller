b899652d0e51fb24deeb718f3205929e8bfc9566
Sammojr <sammojr614@gmail.com>
2020-02-19 10:46:02 -0500

    Lamp Script

---
+++ Descent Into Ere/Assets/Scripts/Random/Lamp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+    public GameObject LampLight;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            LampLight.SetActive(true);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            LampLight.SetActive(false);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            LampLight.SetActive(false);
+        }
+    }
+}
