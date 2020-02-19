a3b3a26628803aec1fb88299d957853ef67575ec
Epiczd <nutball37@gmail.com>
2020-02-19 12:09:14 -0500

    Sprint Script

---
+++ Descent Into Ere/Assets/Scripts/Player/Sprint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprint : MonoBehaviour
+{
+    //Speed the player moves while sprinting
+    private float sprintSpeed = 10f;
+
+    /* If the player is on the ground, and has enough stamina (will be added later). 
+     * Then while the player is holding the sprint button, the player will sprint
+     */
+    void Update()
+    {
+        while(Input.GetButton("Sprint") && PlayerMovement.isGrounded == true)
+        {
+            Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            transform.position += Movement * Time.deltaTime * sprintSpeed;
+            break;
+        }
+    }
+}
