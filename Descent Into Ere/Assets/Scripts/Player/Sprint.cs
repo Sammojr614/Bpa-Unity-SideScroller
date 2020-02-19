0caf8109296488236188c3bfa1a40f871b6630f7
Epiczd <nutball37@gmail.com>
2020-02-19 12:40:28 -0500

    Stamina Stuff

--- Descent Into Ere/Assets/Scripts/Player/Sprint.cs
+++ Descent Into Ere/Assets/Scripts/Player/Sprint.cs
@@ -7,16 +7,47 @@ public class Sprint : MonoBehaviour
     //Speed the player moves while sprinting
     private float sprintSpeed = 10f;
 
-    /* If the player is on the ground, and has enough stamina (will be added later). 
-     * Then while the player is holding the sprint button, the player will sprint
+    //Total stamina the player has
+    public static float stamina = 10f;
+
+    //Determines if the player is sprinting or not
+    private bool sprinting;
+
+    /* If the player is on the ground, and has enough stamina. 
+     * Then while the player is holding the sprint button, the player will sprint.
+     * If the player is NOT grounded, or does NOT have enough stamina, they cannot sprint.
      */
     void Update()
     {
-        while(Input.GetButton("Sprint") && PlayerMovement.isGrounded == true)
+        Debug.Log(stamina);
+
+        if(Input.GetButton("Sprint") && PlayerMovement.isGrounded == true && stamina > 0f)
         {
+            stamina -= Time.deltaTime * 5;
+
+            sprinting = true;
+
             Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
             transform.position += Movement * Time.deltaTime * sprintSpeed;
-            break;
+        }
+        else
+        {
+            sprinting = false;
+        }
+
+        
+        /* If the player has less than 10 stamina, and is NOT sprinting,
+         * the player will begin to regenerate stamina.
+         * If the player has equal or higher than 10 stamina,
+         * the player's stamina will be set equal to 10.
+         */
+        if(stamina < 10f && sprinting == false)
+        {
+            stamina += Time.deltaTime * 5;
+        }
+        else if(stamina > 10f)
+        {
+            stamina = 10f;
         }
     }
 }
