b10f3e09d7c24f27af6bbde9ef12a687faa786d4
Sammojr <sammojr614@gmail.com>
2020-02-21 10:44:54 -0500

    Stamina Bar

--- Descent Into Ere/Assets/Scripts/Animation/Movement.cs
+++ Descent Into Ere/Assets/Scripts/Animation/Movement.cs
@@ -7,10 +7,11 @@ public class Movement : MonoBehaviour
     public Animator PlayerAnimator;
     private void Update()
     {
-        if (Input.GetButton("Horizontal"))
+        Debug.Log(PlayerAnimator.GetFloat("Movement"));
+        if (Input.GetButton("Horizontal") && !Input.GetButton("Sprint"))
         {
             PlayerAnimator.SetFloat("Movement", 0.5f);
-        }else if(Input.GetButton("Horizontal") && Input.GetButton("Sprint"))
+        }else if(Input.GetButton("Sprint") && Input.GetButton("Horizontal"))
         {
             PlayerAnimator.SetFloat("Movement", 1.0f);
         }
@@ -18,6 +19,16 @@ public class Movement : MonoBehaviour
         {
             PlayerAnimator.SetFloat("Movement", 0.0f);
         }
-
+        if (Input.GetButtonDown("MoveRight"))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+           
+        }
+        if (Input.GetButtonDown("MoveLeft"))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            
+        }
+        
     }
 }
---
+++ Descent Into Ere/Assets/Scripts/U.I/Stamina.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stamina : MonoBehaviour
+{
+    const int totalStamina = 10;
+    public GameObject[] StaminaCharges;
+    private void Update()
+    {
+        if(Sprint.stamina == 10)
+        {
+            foreach(GameObject charge in StaminaCharges)
+            {
+                charge.SetActive(true);
+            }
+        }
+        
+        //First Charge Gone
+        if (Sprint.stamina <= 8.5) 
+        {
+            StaminaCharges[0].SetActive(false);
+        }else if(Sprint.stamina <= 7)
+        {
+            //Second Charge Gone
+            StaminaCharges[1].SetActive(false);
+        }else if(Sprint.stamina <= 6.5)
+        { //Third Charge Gone
+            StaminaCharges[2].SetActive(false);
+        }else if(Sprint.stamina <= 3)
+        {
+            //Forth Charge Gone
+            StaminaCharges[3].SetActive(false);
+        }else if(Sprint.stamina <= 2.5)
+        {
+            //Fifth Charge Gone
+            StaminaCharges[4].SetActive(false);
+        }else if(Sprint.stamina <= 1)
+        {
+            //Last Charge Gone
+            StaminaCharges[5].SetActive(false);
+        }
+        if (Sprint.stamina <= 0)
+        {
+            foreach (GameObject charge in StaminaCharges)
+            {
+                charge.SetActive(false);
+            }
+        }
+    }
+}
