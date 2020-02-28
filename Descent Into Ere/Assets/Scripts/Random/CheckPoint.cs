b3350a8a67643af5fc2c092c38eb8aa021b6293c
Epiczd <nutball37@gmail.com>
2020-02-28 18:03:52 -0500

    U.I. Fixes, and CheckPoint Active Sprite

--- Descent Into Ere/Assets/Scripts/Random/CheckPoint.cs
+++ Descent Into Ere/Assets/Scripts/Random/CheckPoint.cs
@@ -8,15 +8,29 @@ public class CheckPoint : MonoBehaviour
     [SerializeField] protected Transform SpawnPointPos;
     [SerializeField] protected Transform CheckPointPos;
 
+    //SpriteRenderers for the flag, active and unactive
+    [SerializeField] private SpriteRenderer flagUnactive;
+    [SerializeField] private SpriteRenderer flagActive;
+
     //Checks if the checkpoint has been reached, this is false by default
     protected static bool checkPointActive = false;
 
+    //On Start, the flag is unactive
+    private void Start()
+    {
+        flagUnactive.enabled = true;
+        flagActive.enabled = false;
+    }
     /* When the player reaches the checkpoint,
-     * it updates the spawn point, and sets checkPointActive to true
+     * it updates the spawn point, and sets checkPointActive to true.
+     * It also changes the flag's sprite,
+     * from unActive, to active.
      */
     private void OnTriggerEnter2D(Collider2D collision)
     {
         SpawnPointPos.position = CheckPointPos.position;
+        flagUnactive.enabled = false;
+        flagActive.enabled = true;
         checkPointActive = true;
     }
 
