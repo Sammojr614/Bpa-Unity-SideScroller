dfbd1b6dc204773b13dd955a4fa6084972ef0331
Epiczd <nutball37@gmail.com>
2020-03-02 12:56:09 -0500

    FallingPlatform script

--- Descent Into Ere/Assets/Scripts/U.I/CurrencyDisplay.cs
+++ Descent Into Ere/Assets/Scripts/U.I/CurrencyDisplay.cs
@@ -17,6 +17,5 @@ public class CurrencyDisplay : MonoBehaviour
         //This just makes it so The Int From the database is changed into a string so the Text Feild can Use it
         CurrencyDisplayText.text = dataMgr.PlayerCurrency.ToString();
         CurrencyDisplayText.text = Shop.PlayerCrystals.ToString();
-        Debug.Log(Shop.PlayerCrystals);
     }
 }
