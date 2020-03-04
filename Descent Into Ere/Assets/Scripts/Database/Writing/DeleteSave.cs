77862309c8046c4182cd6b60cc7b99255328b3de
Sammmojr <sammojr614@gmail.com>
2020-03-04 13:41:12 -0500

    Delete Save Script

---
+++ Descent Into Ere/Assets/Scripts/Database/Writing/DeleteSave.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeleteSave : MonoBehaviour
+{
+    public Button DropButton;
+    DbManager dbMgr = DbManager.Instance;
+    private void Start()
+    {
+        DropButton.onClick.AddListener(DeleteOnClick);
+    }
+    void DeleteOnClick()
+    {
+        dbMgr.normalDbCommand("DROP TABLE PlayerSaveData");
+        dbMgr.normalDbCommand("DROP TABLE LevelIndex");
+        dbMgr.normalDbCommand("DROP TABLE PlayerInventory");
+    }
+}
