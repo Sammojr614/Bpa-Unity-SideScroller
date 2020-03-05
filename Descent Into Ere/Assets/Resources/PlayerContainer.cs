7e4b65c2d46de235e4d7d27fe37e34faa83b25de
Sammmojr <sammojr614@gmail.com>
2020-03-05 18:50:01 -0500

    XmlAttempt

---
+++ Descent Into Ere/Assets/Resources/PlayerContainer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+[XmlRoot("PlayerDataCollection")]
+public class PlayerContainer 
+{
+    [XmlArray("Player")]
+    [XmlArrayItem("Health")]
+   public List<Player> PlayerSaveData =  new List<Player>();
+    public static PlayerContainer Load(string path)
+    {
+        TextAsset _xml = Resources.Load<TextAsset>(path);
+        XmlSerializer dataFinder = new XmlSerializer(typeof(PlayerContainer));
+        StringReader reader = new StringReader(_xml.text);
+        PlayerContainer player = dataFinder.Deserialize(reader) as PlayerContainer;
+        reader.Close();
+        return player;
+    }
+   
+}
