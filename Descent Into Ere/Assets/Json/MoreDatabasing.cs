50cc25ed47a675afb5efc9a178a70f493c44fad3
Sammojr <sammojr614@gmail.com>
2020-03-05 22:58:46 -0500

    Json is Supiror

---
+++ Descent Into Ere/Assets/Json/MoreDatabasing.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoreDatabasing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerData Save = new PlayerData();
+        Save.Health = 10;
+        Save.Lives = 333;
+        string json = JsonUtility.ToJson(Save);
+       PlayerData loaded =  JsonUtility.FromJson<PlayerData>(json);
+        Debug.Log("Health Test: Health = " + loaded.Health);
+    }
+
+    private class PlayerData
+    {
+        public int Health;
+        public int Lives;
+    }
+}
