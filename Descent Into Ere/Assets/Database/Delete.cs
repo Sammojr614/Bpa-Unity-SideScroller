8f9a2aa1b83d92e99eddd47c560b414a465121a6
Sammmojr <sammojr614@gmail.com>
2020-03-10 08:30:32 -0400

    Changes to U.I

---
+++ Descent Into Ere/Assets/Database/Delete.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Delete : MonoBehaviour
+{
+    public Button EraseButton;
+    public GameObject EraseDialog;
+    public Button YesButton;
+    public Button NoButton;
+    void Start()
+    {
+        EraseDialog.SetActive(false);
+        EraseButton.onClick.AddListener(OpenOnClick);
+        NoButton.onClick.AddListener(CloseOnClick);
+        YesButton.onClick.AddListener(CloseOnClick);
+    }
+    void OpenOnClick()
+    {
+        EraseDialog.SetActive(true);
+    }
+    void CloseOnClick()
+    {
+        EraseDialog.SetActive(false);
+    }
+
+   
+}
