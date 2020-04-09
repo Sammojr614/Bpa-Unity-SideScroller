abeb907233d99c5c23f4a95d96642d013830a3c3
Sammmojr <sammojr614@gmail.com>
2020-04-09 10:36:18 -0400

    New Code for new Data

--- Descent Into Ere/Assets/Scripts/DataBaseScripts/Writing/NewData.cs
+++ Descent Into Ere/Assets/Scripts/DataBaseScripts/Writing/NewData.cs
@@ -3,29 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class NewData : MonoBehaviour
 {
     public Button CreateButton;
     DataMgr Data = DataMgr.Instance;
     InventoryMgr Inven = InventoryMgr.Instance;
-    
-    private void Start()
+    public GameObject haveyouPlayedBefore;
+    public Button yesButton;
+    public Button noButton;
+
+   void Start()
     {
-        CreateButton.onClick.AddListener(taskOnClick);
+        CreateButton.onClick.AddListener(createOnClick);
+        yesButton.onClick.AddListener(YesClick);
+        noButton.onClick.AddListener(noOnClick);
     }
-    void taskOnClick()
+    void createOnClick()
     {
-        Data.TimesPlayed = 0;
-        Data.location = "MainHub";
         Data.Playerhealth = 4;
         Data.PlayerLives = 3;
-        Inven.NumberOfItem = 0;
+        Data.Playercurrency = 0;
+        Data.TimesPlayed = 1;
+        string newData = JsonUtility.ToJson(Data);
         Inven.ItemName = "Potion";
-        string jsonData = JsonUtility.ToJson(Data);
-        File.WriteAllText(Application.dataPath +  "PlayerSaveData.json",jsonData);
-        string jsonInven = JsonUtility.ToJson(Inven);
-        File.WriteAllText(Application.dataPath + "PlayerInventory.json", jsonInven);
-        
+        Inven.NumberOfItem = 0;
+        string invenData = JsonUtility.ToJson(Inven);
+        //writing to the Json File
+        File.WriteAllText(Application.dataPath + "PlayerSaveData.json", newData);
+        File.WriteAllText("PlayerInventory.json", invenData);
+        haveyouPlayedBefore.SetActive(true);
+    }
+    void YesClick()
+    {
+        SceneManager.LoadScene("MainHub");
+    }
+    void noOnClick()
+    {
+        SceneManager.LoadScene("Tutorial");
     }
 }
